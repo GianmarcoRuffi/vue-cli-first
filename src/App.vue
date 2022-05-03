89468e5f8bfabde8f329b0ce1a664785600f9818
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-05-03 18:22:19 +0200

    Aggiunto Hero

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <MyTitle />
+    <MyHero />
   </div>
 </template>
 
 <script>
 import MyTitle from "./components/MyTitle.vue";
+import MyHero from "./components/MyHero.vue";
 
 export default {
   name: "App",
   components: {
     MyTitle,
+    MyHero,
   },
 };
 </script>
